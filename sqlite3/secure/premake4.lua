34f680386354ce25e3f1e85cac582883c995767b
Fadhil Mandaga <firodj@gmail.com>
2015-02-06 19:50:22 +0700

    works premake4

--- sqlite3/secure/premake4.lua
+++ sqlite3/secure/premake4.lua
@@ -4,7 +4,7 @@
 --   Debug or Release
 
 solution "SQLite3"
-  configurations { "Debug AES128", "Release AES128", "Debug AES256", "Release AES256" }
+  configurations { "DebugAES128", "ReleaseAES128", "DebugAES256", "ReleaseAES256" }
 
 -- SQLite3 as static library
 project "sqlite3lib"
@@ -16,14 +16,12 @@ project "sqlite3lib"
     ["Source Files"] = { "**.c" }
   }
   files { "src/sqlite3secure.c", "src/*.h" }
-  flags { "Unicode", "StaticRuntime" }  
+  flags { "Unicode", "StaticRuntime" }
 
   location "build/sqlite3lib"
   targetname "sqlite3"
 
   defines {
-    "WIN32",
-    "_WINDOWS",
     "_LIB",
     "THREADSAFE=1",
     "SQLITE_SOUNDEX",
@@ -36,45 +34,56 @@ project "sqlite3lib"
     "SQLITE_CORE",
     "SQLITE_ENABLE_EXTFUNC",
     "SQLITE_USE_URI",
-	"SQLITE_USER_AUTHENTICATION",
+    "SQLITE_USER_AUTHENTICATION",
     "_CRT_SECURE_NO_WARNINGS",
     "_CRT_SECURE_NO_DEPRECATE",
     "_CRT_NONSTDC_NO_DEPRECATE"
   }
 
-  configuration "Debug AES128"
+  configuration "windows"
+    defines { "WIN32", "_WINDOWS" }
+
+  configuration "linux"
+    defines {
+      "HAVE_ACOSH=1",
+      "HAVE_ASINH=1",
+      "HAVE_ATANH=1",
+      "HAVE_ISBLANK=1"
+    }
+
+  configuration "DebugAES128"
     targetdir "aes128/lib/debug"
     defines {
       "CODEC_TYPE=CODEC_TYPE_AES128",
-      "DEBUG", 
+      "DEBUG",
       "_DEBUG"
     }
     flags { "Symbols" }
- 
-  configuration "Release AES128"
-    targetdir "aes128/lib/release"
-    defines { 
-      "CODEC_TYPE=CODEC_TYPE_AES128",
-      "NDEBUG" 
-    }
-    flags { "Optimize" }  
 
-    configuration "Debug AES256"
+  configuration "ReleaseAES128"
+    targetdir "aes128/lib/release"
+    defines {
+      "CODEC_TYPE=CODEC_TYPE_AES128",
+      "NDEBUG"
+    }
+    flags { "Optimize" }
+
+    configuration "DebugAES256"
     targetdir "aes256/lib/debug"
     defines {
       "CODEC_TYPE=CODEC_TYPE_AES256",
-      "DEBUG", 
-      "_DEBUG" 
+      "DEBUG",
+      "_DEBUG"
     }
     flags { "Symbols" }
- 
-  configuration "Release AES256"
+
+  configuration "ReleaseAES256"
     targetdir "aes256/lib/release"
-    defines { 
+    defines {
       "CODEC_TYPE=CODEC_TYPE_AES256",
-      "NDEBUG" 
+      "NDEBUG"
     }
-    flags { "Optimize" }  
+    flags { "Optimize" }
 
 -- SQLite3 as shared library
 project "sqlite3dll"
@@ -85,15 +94,13 @@ project "sqlite3dll"
     ["Header Files"] = { "**.h" },
     ["Source Files"] = { "**/sqlite3secure.c", "**.def", "**.rc" }
   }
-  files { "src/sqlite3secure.c", "src/*.h", "src/sqlite3.def", "src/sqlite3.rc" }
-  flags { "Unicode", "StaticRuntime" }  
+  files { "src/sqlite3secure.c", "src/*.h", "src/sqlite3.def" }
+  flags { "Unicode", "StaticRuntime" }
 
   location "build/sqlite3dll"
   targetname "sqlite3"
 
   defines {
-    "WIN32",
-    "_WINDOWS",
     "_USRDLL",
     "THREADSAFE=1",
     "SQLITE_SOUNDEX",
@@ -106,47 +113,59 @@ project "sqlite3dll"
     "SQLITE_CORE",
     "SQLITE_ENABLE_EXTFUNC",
     "SQLITE_USE_URI",
-	"SQLITE_USER_AUTHENTICATION",
+    "SQLITE_USER_AUTHENTICATION",
     "_CRT_SECURE_NO_WARNINGS",
     "_CRT_SECURE_NO_DEPRECATE",
     "_CRT_NONSTDC_NO_DEPRECATE"
   }
 
-  configuration "Debug AES128"
+  configuration "windows"
+    files { "src/sqlite3.rc" }
+    defines { "WIN32", "_WINDOWS" }
+
+  configuration "linux"
+    defines {
+      "HAVE_ACOSH=1",
+      "HAVE_ASINH=1",
+      "HAVE_ATANH=1",
+      "HAVE_ISBLANK=1"
+    }
+
+  configuration "DebugAES128"
     targetdir "aes128/dll/debug"
     defines {
       "CODEC_TYPE=CODEC_TYPE_AES128",
-      "DEBUG", 
+      "DEBUG",
       "_DEBUG"
     }
     flags { "Symbols" }
- 
-  configuration "Release AES128"
-    targetdir "aes128/dll/release"
-    defines { 
-      "CODEC_TYPE=CODEC_TYPE_AES128",
-      "NDEBUG" 
-    }
-    flags { "Optimize" }  
 
-  configuration "Debug AES256"
+  configuration "ReleaseAES128"
+    targetdir "aes128/dll/release"
+    defines {
+      "CODEC_TYPE=CODEC_TYPE_AES128",
+      "NDEBUG"
+    }
+    flags { "Optimize" }
+
+  configuration "DebugAES256"
     targetdir "aes256/dll/debug"
     defines {
       "CODEC_TYPE=CODEC_TYPE_AES256",
-      "DEBUG", 
-      "_DEBUG" 
+      "DEBUG",
+      "_DEBUG"
     }
     flags { "Symbols" }
- 
-  configuration "Release AES256"
-    targetdir "aes256/dll/release"
-    defines { 
-      "CODEC_TYPE=CODEC_TYPE_AES256",
-      "NDEBUG" 
-    }
-    flags { "Optimize" }  
 
--- SQLite3 Shell    
+  configuration "ReleaseAES256"
+    targetdir "aes256/dll/release"
+    defines {
+      "CODEC_TYPE=CODEC_TYPE_AES256",
+      "NDEBUG"
+    }
+    flags { "Optimize" }
+
+-- SQLite3 Shell
 project "sqlite3shell"
   uuid "BA98AAC1-AACD-2F4F-8EDB-CF7C62668BC4"
   kind "ConsoleApp"
@@ -155,37 +174,42 @@ project "sqlite3shell"
     ["Header Files"] = { "**.h" },
     ["Source Files"] = { "**.c", "**.rc" }
   }
-  files { "src/sqlite3.h", "src/shell.c", "src/sqlite3shell.rc" }
-  flags { "Unicode", "StaticRuntime" }  
+  files { "src/sqlite3.h", "src/shell.c" }
+  flags { "Unicode", "StaticRuntime" }
   links { "sqlite3lib" }
 
   location "build/sqlite3shell"
 
   defines {
-    "WIN32",
-    "_WINDOWS",
     "SQLITE_HAS_CODEC",
-	"SQLITE_USER_AUTHENTICATION",
+    "SQLITE_USER_AUTHENTICATION",
     "_CRT_SECURE_NO_DEPRECATE",
     "_CRT_NONSTDC_NO_DEPRECATE"
   }
 
-  configuration "Debug AES128"
-    targetdir "aes128/lib/debug"
-    defines { "DEBUG" }
-    flags { "Symbols" }
- 
-  configuration "Release AES128"
-    targetdir "aes128/lib/release"
-    defines { "NDEBUG" }
-    flags { "Optimize" }  
+  configuration "windows"
+    files { "src/sqlite3shell.rc" }
+    defines { "WIN32", "_WINDOWS" }
 
-  configuration "Debug AES256"
-    targetdir "aes256/lib/debug"
+  configuration "linux"
+    links { "pthread", "dl" }
+
+  configuration "DebugAES128"
+    targetdir "aes128/debug"
     defines { "DEBUG" }
     flags { "Symbols" }
- 
-  configuration "Release AES256"
-    targetdir "aes256/lib/release"
+
+  configuration "ReleaseAES128"
+    targetdir "aes128/release"
     defines { "NDEBUG" }
-    flags { "Optimize" }  
+    flags { "Optimize" }
+
+  configuration "DebugAES256"
+    targetdir "aes256/debug"
+    defines { "DEBUG" }
+    flags { "Symbols" }
+
+  configuration "ReleaseAES256"
+    targetdir "aes256/release"
+    defines { "NDEBUG" }
+    flags { "Optimize" }
